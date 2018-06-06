b1b153ca69a34f35a72c3cc33c96fe654015d6f5
NULL-LI <lixiang3.1416@qq.com>
2018-06-06 17:35:59 +0800

    init make

---
+++ qt-build/src/CMakeFiles/myslam.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/null/slam/mono-slam-vo/mono-slam-vo/src/camera.cpp" "/home/null/slam/mono-slam-vo/mono-slam-vo/qt-build/src/CMakeFiles/myslam.dir/camera.cpp.o"
+  "/home/null/slam/mono-slam-vo/mono-slam-vo/src/config.cpp" "/home/null/slam/mono-slam-vo/mono-slam-vo/qt-build/src/CMakeFiles/myslam.dir/config.cpp.o"
+  "/home/null/slam/mono-slam-vo/mono-slam-vo/src/frame.cpp" "/home/null/slam/mono-slam-vo/mono-slam-vo/qt-build/src/CMakeFiles/myslam.dir/frame.cpp.o"
+  "/home/null/slam/mono-slam-vo/mono-slam-vo/src/g2o_types.cpp" "/home/null/slam/mono-slam-vo/mono-slam-vo/qt-build/src/CMakeFiles/myslam.dir/g2o_types.cpp.o"
+  "/home/null/slam/mono-slam-vo/mono-slam-vo/src/map.cpp" "/home/null/slam/mono-slam-vo/mono-slam-vo/qt-build/src/CMakeFiles/myslam.dir/map.cpp.o"
+  "/home/null/slam/mono-slam-vo/mono-slam-vo/src/mappoint.cpp" "/home/null/slam/mono-slam-vo/mono-slam-vo/qt-build/src/CMakeFiles/myslam.dir/mappoint.cpp.o"
+  "/home/null/slam/mono-slam-vo/mono-slam-vo/src/visual_odometry.cpp" "/home/null/slam/mono-slam-vo/mono-slam-vo/qt-build/src/CMakeFiles/myslam.dir/visual_odometry.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  "/home/null/slam/slambook-master/3rdparty/Sophus"
+  "../include"
+  "../Thirdparty/g2o"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ qt-build/test/CMakeFiles/gentxt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gentxt.dir/gentxt.cpp.o"
+  "../../bin/gentxt.pdb"
+  "../../bin/gentxt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gentxt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
