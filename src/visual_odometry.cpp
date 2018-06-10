04a49927ed25b77b556e9eabfd8c7a11913565a4
NULL-LI <lixiang3.1416@qq.com>
2018-06-10 12:33:04 +0800

    backup

--- src/visual_odometry.cpp
+++ src/visual_odometry.cpp
@@ -941,10 +941,10 @@ void VisualOdometry::triangulation()
         }
         else
         {
-                        if(l1+l2>30)
-                        {
-                            continue;
-                        }
+//                        if(l1+l2>30)
+//                        {
+//                            continue;
+//                        }
 
             //            if(angleRad>min_view_angle_triangulation_&&error_cur<max_mean_view_error_triangulation_&&error_ref<max_mean_view_error_triangulation_)
             //            if((error_ref*error_cur/pow(angleRad,3.2))<max_mean_view_error_triangulation_*max_mean_view_error_triangulation_/pow(min_view_angle_triangulation_,3.2))
@@ -1204,7 +1204,7 @@ void VisualOdometry::addMapPointsTriangulation()
         //            matched2d2d[index] = true;
         int NewPointsNum=0;
 
-        double densityRatio=24.0;
+        double densityRatio=8.0;
         Mat PositionTable = Mat::zeros(ref_->color_.cols/densityRatio,ref_->color_.rows/densityRatio, CV_8UC1);
         cout<<"PositionTable.size : "<<PositionTable.size<<endl;
 
@@ -1290,6 +1290,7 @@ void VisualOdometry::optimizeMap()
 {
     // remove the hardly seen and no visible points
     int k1=0;
+    int erase_cnt=0;
     for ( auto iter = map_->map_points_.begin(); iter != map_->map_points_.end(); )
     {
         if ( !ref_->isInFrame(iter->second->pos_) /*&&k1%10!=0*/)
@@ -1321,11 +1322,12 @@ void VisualOdometry::optimizeMap()
             continue;
         }
 
-//        if ( iter->second->matched_times_ > 15 )
-//        {
-//            iter = map_->map_points_.erase(iter);
-//            continue;
-//        }
+        if ( iter->second->matched_times_ > 10&&erase_cnt%3==0 )
+        {
+            iter = map_->map_points_.erase(iter);
+            erase_cnt++;
+            continue;
+        }
 
         if ( iter->second->unmatched_times_ >4 )
         {
@@ -1380,7 +1382,7 @@ void VisualOdometry::globalBundleAdjustment()
 
     long unsigned int maxKFid = 0;
     long unsigned int minKFid = 999999;
-    int deltaf = 10;
+    int deltaf = 11;
 
     for(auto iter = map_->keyframes_.begin(); iter != map_->keyframes_.end();iter++)
     {
